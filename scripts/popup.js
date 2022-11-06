a7ef72ad47bbab62a9f67882045f67d14c1e43b7
James Crutchley <ne0rmatrix@gmail.com>
2022-11-06 11:42:17 -0800

    small fixes

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,13 +1,24 @@
 var counter = 0;
 var isChecked = 'yes';
 
-chrome.runtime.sendMessage({question:"Counter"}, function(response) 
+chrome.runtime.sendMessage({question: 'Counter'}, function(response) 
 {
-  savedCounter = response.SendingCounter;
+  counter = response.SendingCounter;
   console.log(response.SendingCounter);
   filter();
   changed(response.SendingCounter);
 });
+
+function getIsChecked()
+{
+		chrome.runtime.sendMessage({question:"ischeck"}, function(response) 
+	{
+		console.log('Received ischeck! ' + response.Sendingischeck);	
+		ischecked = response.Sendingischeck; 
+	});
+  console.log(isChecked)
+};
+getIsChecked();
 
  function SendStatus(status)
  {
@@ -16,7 +27,10 @@ chrome.runtime.sendMessage({question:"Counter"}, function(response)
 document.body.onload = function() 
 {
   
-
+  radiobtn = document.getElementById("spanner");
+  if (isChecked == "yes") radiobtn.checked = true;
+  else radiobtn.checked = false;
+  //document.getElementById(btn).append.radiobtn.checked
   let h2 = document.createElement('h2');
   let tbody = document.createElement('tbody');
   let text = document.createTextNode('Authors Blocked');

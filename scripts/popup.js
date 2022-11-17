894a8a8001c9ea60eb679f83ecdb3583178e1faf
James Crutchley <ne0rmatrix@gmail.com>
2022-11-17 03:13:59 -0800

    Popup fixed for eslint

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,95 +1,92 @@
+/* eslint-disable no-use-before-define */
 /* global chrome */
 
-const getImports = () => {
-  return import(
-    (chrome.runtime.getURL || chrome.extension.getURL)('/scripts/settings.js')
-  )
-}
+const getImports = () => import(
+  (chrome.runtime.getURL || chrome.extension.getURL)('/scripts/settings.js')
+);
 
 const importSettings = async () => {
-  const result = await (await getImports()).loadSettings()
-  return result
-}
+  const result = await (await getImports()).loadSettings();
+  return result;
+};
 document.getElementById('options').addEventListener('click', () => {
   if (chrome.runtime.openOptionsPage) {
-    chrome.runtime.openOptionsPage()
+    chrome.runtime.openOptionsPage();
   } else {
-    window.open(chrome.runtime.getURL('options.html'))
+    window.open(chrome.runtime.getURL('options.html'));
   }
-})
+});
 
 document.getElementById('btn').addEventListener('click', () => {
-  const span = document.getElementById('btn')
+  const span = document.getElementById('btn');
   if (span.checked) {
-    SendStatus('yes')
-    load()
+    SendStatus('yes');
+    load();
   } else {
-    SendStatus('no')
-    load()
+    SendStatus('no');
+    load();
   }
-})
+});
 
 document.body.onload = async () => {
-  load()
-}
+  load();
+};
 
 const load = async () => {
-  const response = await getSettings({ question: 'settings' })
-  const settings = await importSettings()
+  const response = await getSettings({ question: 'settings' });
+  const settings = await importSettings();
   settings.addAll(
     response.counter,
     response.currentUrl,
     response.ischeck,
-    response.author
-  )
+    response.author,
+  );
   createTable(
     settings.getIschecked(),
     settings.getCounter(),
-    settings.getUrl()
-  )
-}
+    settings.getUrl(),
+  );
+};
 const filter = async () => {
-  const arr = document.querySelector('div')
-  arr.innerHTML = ''
-}
+  const arr = document.querySelector('div');
+  arr.innerHTML = '';
+};
 
-const getSettings = async (msg) => {
-  return new Promise((resolve, reject) => {
-    chrome.runtime.sendMessage(msg, function (response) {
-      if (typeof response.SendingSettings === 'undefined') reject(console.log('Failed to send authors!'))
-      resolve(response.SendingSettings)
-    })
-  })
-}
+const getSettings = async (msg) => new Promise((resolve, reject) => {
+  chrome.runtime.sendMessage(msg, (response) => {
+    if (typeof response.SendingSettings === 'undefined') reject(console.log('Failed to send authors!'));
+    resolve(response.SendingSettings);
+  });
+});
 
 const createTable = async (isChecked, counter, currrentUrl) => {
-  filter()
-  let temp = 0
+  filter();
+  let temp = 0;
 
-  const span = document.getElementById('btn')
+  const span = document.getElementById('btn');
   if (isChecked === 'yes') {
-    span.checked = true
-  } else span.checked = false
+    span.checked = true;
+  } else span.checked = false;
   if (
-    isChecked === 'yes' &&
-    currrentUrl.includes('amazon') &&
-    typeof counter !== 'undefined'
-  ) { temp = counter }
+    isChecked === 'yes'
+    && currrentUrl.includes('amazon')
+    && typeof counter !== 'undefined'
+  ) { temp = counter; }
 
-  let h2 = document.createElement('h2')
-  const tbody = document.createElement('tbody')
-  const text = document.createTextNode('Authors Blocked')
-  const numbersText = document.createTextNode(temp)
+  let h2 = document.createElement('h2');
+  const tbody = document.createElement('tbody');
+  const text = document.createTextNode('Authors Blocked');
+  const numbersText = document.createTextNode(temp);
 
-  h2.appendChild(text)
-  tbody.appendChild(h2)
-  h2 = document.createElement('h2')
-  h2.appendChild(numbersText)
-  tbody.appendChild(h2)
+  h2.appendChild(text);
+  tbody.appendChild(h2);
+  h2 = document.createElement('h2');
+  h2.appendChild(numbersText);
+  tbody.appendChild(h2);
 
-  document.getElementById('AuthorsBlocked').appendChild(tbody)
-}
+  document.getElementById('AuthorsBlocked').appendChild(tbody);
+};
 
 const SendStatus = (status) => {
-  chrome.runtime.sendMessage({ SendingIsChecked: status })
-}
+  chrome.runtime.sendMessage({ SendingIsChecked: status });
+};
